b094b745cec3ed2701f3835485bc8d66b312510b
Natalia <mayersita.mayer@gmail.com>
2020-09-16 21:55:23 -0300

    Ajuste no cadastro de usuario pra devolver o token

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -18,12 +18,24 @@ class UserController {
   }
 
   async store(req, res) {
-    const { email } = req.body;
+    const { email, password } = req.body;
     if (await User.findOne({ email })) {
       return res.status(400).json({ error: 'User already exists' });
     }
     const user = await User.create(req.body);
-    return res.json(user);
+
+    const userWithToken = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+    if (!(await userWithToken.compareHash(password))) {
+      return res.status(400).json({ error: 'Invalid password' });
+    }
+
+    return res.json({
+      userWithToken,
+      token: User.generateToken(userWithToken),
+    });
   }
 
   async update(req, res) {
